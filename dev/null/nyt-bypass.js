b764d3765c697ccef084882617cba9e861979bfc
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-08-08 06:18:11 -0500

    Move paywall bypasses to bookmarklets repo

--- dev/null/nyt-bypass.js
+++
@@ -1,2 +0,0 @@
-// Bypasses New Yprk Times paywall
-javascript:document.cookie="nyt-m=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.nytimes.com;path=/";location.reload()
--- dev/null/wired-bypass.js
+++
@@ -1,2 +0,0 @@
-// Bypass WIRED paywall
-javascript:document.cookie="pay_ent_smp=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=.wired.com;path=/";location.reload()
