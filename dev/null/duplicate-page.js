d2831014a0668df3b7df109741b403306d7379e3
James Martindale <11380394+jkmartindale@users.noreply.github.com>
2021-08-08 06:18:47 -0500

    Not my code

--- dev/null/duplicate-page.js
+++
@@ -1,7 +0,0 @@
-javascript:
-nd = window.open().document;
-nd.head.innerHTML = document.head.innerHTML;
-base = document.createElement("body");
-base.href = window.location.href;
-nd.head.appendChild(base);
-nd.body.innerHTML = document.body.innerHTML;
